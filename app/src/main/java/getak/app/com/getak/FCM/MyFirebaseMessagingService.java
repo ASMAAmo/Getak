12027d05ee569b2089671fa7108433d9ef357263
ASMAAmo <asmaa_mostafa24@yahoo.com>
2019-04-13 16:59:12 +0200

    Map and firebase

---
+++ app/src/main/java/getak/app/com/getak/FCM/MyFirebaseMessagingService.java
@@ -0,0 +1,126 @@
+package getak.app.com.getak.FCM;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import getak.app.com.getak.R;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
+    private NotificationChannel mChannel;
+    private NotificationManager notifManager;
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        try {
+            Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(this, alarmSound);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.e(TAG, "Data Payload: " + remoteMessage.getData().toString());
+            String title = "0";
+            String body = "0";
+            String message = "0";
+            title = remoteMessage.getData().get("title");
+            body =  remoteMessage.getData().get("body");
+            message = remoteMessage.getData().get("message");
+
+            Log.e(TAG, "title: " + title);
+            Log.e(TAG, "message: " + body);
+            Log.e(TAG, "id: " + message);
+            String click_action = remoteMessage.getNotification().getClickAction();
+            notify(title,body,message,click_action);
+        }
+    }
+
+
+
+
+    public void notify(String title, String description, String message, String click_action) {
+
+        if (notifManager == null) {
+            notifManager = (NotificationManager) getSystemService
+                    (Context.NOTIFICATION_SERVICE);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationCompat.Builder builder;
+            Intent intent = new Intent(click_action);
+            intent.putExtra("orderId", message);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent;
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            if (mChannel == null) {
+                mChannel = new NotificationChannel
+                        ("0", title, importance);
+                mChannel.setDescription(description);
+                mChannel.enableVibration(true);
+                notifManager.createNotificationChannel(mChannel);
+            }
+            builder = new NotificationCompat.Builder(this, "0");
+
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                    Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(this, 1251, intent, PendingIntent.FLAG_ONE_SHOT);
+            builder.setContentTitle(title)
+                    .setSmallIcon(getNotificationIcon()) // required
+                    .setContentText(description)  // required
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setLargeIcon(BitmapFactory.decodeResource
+                            (getResources(), R.drawable.logo))
+                    .setBadgeIconType(R.drawable.logo)
+                    .setContentIntent(pendingIntent)
+                    .setSound(RingtoneManager.getDefaultUri
+                            (RingtoneManager.TYPE_NOTIFICATION));
+            Notification notification = builder.build();
+            notifManager.notify(0, notification);
+        } else {
+
+            Intent intent = new Intent(click_action);
+            intent.putExtra("orderId", message);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = null;
+
+            pendingIntent = PendingIntent.getActivity(this, 1251, intent, PendingIntent.FLAG_ONE_SHOT);
+
+            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                    .setContentTitle(title)
+                    .setContentText(description)
+                    .setAutoCancel(true)
+                    .setColor(ContextCompat.getColor(getBaseContext(), R.color.colorPrimary))
+                    .setSound(defaultSoundUri)
+                    .setSmallIcon(getNotificationIcon())
+                    .setContentIntent(pendingIntent)
+                    .setStyle(new NotificationCompat.BigTextStyle().setBigContentTitle(title).bigText(description));
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(1251, notificationBuilder.build());
+        }
+    }
+
+    private int getNotificationIcon() {
+        boolean useWhiteIcon = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP);
+        return useWhiteIcon ? R.drawable.logo : R.drawable.logo;
+    }
+}
--- app/src/main/java/getak/app/com/getak/Fragments/FragmentHome.java
+++ app/src/main/java/getak/app/com/getak/Fragments/FragmentHome.java
@@ -1,30 +1,139 @@
 package getak.app.com.getak.Fragments;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import getak.app.com.getak.GpsUtils.GPSTracker;
 import getak.app.com.getak.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class FragmentHome extends Fragment {
+public class FragmentHome extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
 
-    public FragmentHome() {
-        // Required empty public constructor
+    public static GoogleMap map;
+    GoogleApiClient mpiclients;
+    Location mlastLocation;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    LocationManager locationManager;
+    LocationRequest mLocationrequest;
+    GPSTracker gpsTracker ;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mFusedLocationProviderClient = LocationServices
+                .getFusedLocationProviderClient(getActivity());
+        gpsTracker=new GPSTracker(getActivity().getApplicationContext());
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragment_home, container, false);
     }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationrequest = new LocationRequest();
+        mLocationrequest.setInterval(60000);
+        mLocationrequest.setFastestInterval(60000);
+        mLocationrequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mpiclients, mLocationrequest, FragmentHome.this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mlastLocation= location;
+        LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        map.setMyLocationEnabled(true);
+        buildGoogleapiClient();
+        if(gpsTracker.canGetLocation()){
+            LatLng currentLatLng = new LatLng(gpsTracker.getLatitude(),
+                    gpsTracker.getLongitude());
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(currentLatLng,
+                    15);
+            map.moveCamera(update);
+        }
+    }
+
+
+
+
+
+
+
+
+
+    protected synchronized void buildGoogleapiClient() {
+        mpiclients = new GoogleApiClient.Builder(getActivity()).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).addApi(Places.GEO_DATA_API).build();
+        mpiclients.connect();
+    }
+
 
 }
